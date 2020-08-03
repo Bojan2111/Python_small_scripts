b9e08a6593ee65455ea20a6fc00389d8f9dcd8f6
Bojan Adzic <39236370+Bojan2111@users.noreply.github.com>
2020-08-03 16:14:24 +0200

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+n = int(input("Enter number: "))
+na = str(n)
+lst = []
+if n == 1:
+	lst.append(0)
+else:
+	lst.append(0)
+	lst.append(1)
+
+i = len(lst)
+while i<n:
+	a = lst[len(lst) - 1] + lst[len(lst) - 2]
+	lst.append(a)
+	i = len(lst)
+
+lsta = str(lst)
+print("The first " + na + " numbers of Fibonacci sequence are: " + lst)
